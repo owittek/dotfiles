36eedfd1014d67734b283bf974fbe839bde207b1
owittek <Olivier.Wittek@web.de>
2022-12-22 18:11:09 +0000

    Initial commit

---
+++ .config/nvim/lua/user/plugins/init.lua
@@ -0,0 +1,11 @@
+return {
+      {
+        "simrat39/rust-tools.nvim",
+        after = "mason-lspconfig.nvim",
+        config = function()
+          require("rust-tools").setup {
+            server = astronvim.lsp.server_settings "rust_analyzer",
+          }
+        end,
+      },
+}
