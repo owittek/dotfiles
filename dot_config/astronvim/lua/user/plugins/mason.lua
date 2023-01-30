0671b693561ac8c081d354f18414a302e7387ecf
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-01-30 08:19:36 +0100

    fixed dap for python

--- dot_config/astronvim/lua/user/plugins/mason.lua
+++ dot_config/astronvim/lua/user/plugins/mason.lua
@@ -8,7 +8,7 @@ return {
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
-    opts = { ensure_installed = { "codelldb", "debugpy" } },
+    opts = { ensure_installed = { "codelldb", "python" } },
   },
   {
     "jay-babu/mason-null-ls.nvim",
@@ -25,27 +25,27 @@ return {
     },
     config = function(plugin, opts)
       plugin.default_config(opts)
-      require("mason-null-ls").setup_handlers {
+      require("mason-null-ls").setup_handlers({
         prettierd = function()
-          require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with {
+          require("null-ls").register(require("null-ls").builtins.formatting.prettierd.with({
             condition = function(utils)
-              return utils.root_has_file "package.json"
-                or utils.root_has_file ".prettierrc"
-                or utils.root_has_file ".prettierrc.json"
-                or utils.root_has_file ".prettierrc.js"
+              return utils.root_has_file("package.json")
+                or utils.root_has_file(".prettierrc")
+                or utils.root_has_file(".prettierrc.json")
+                or utils.root_has_file(".prettierrc.js")
             end,
-          })
+          }))
         end,
         eslint_d = function()
-          require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with {
+          require("null-ls").register(require("null-ls").builtins.diagnostics.eslint_d.with({
             condition = function(utils)
-              return utils.root_has_file "package.json"
-                or utils.root_has_file ".eslintrc.json"
-                or utils.root_has_file ".eslintrc.js"
+              return utils.root_has_file("package.json")
+                or utils.root_has_file(".eslintrc.json")
+                or utils.root_has_file(".eslintrc.js")
             end,
-          })
+          }))
         end,
-      }
+      })
     end,
   },
 }
