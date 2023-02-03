9b3237ce84df368c780f1d6ed4512bfce9d3d270
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-02-03 19:40:42 +0100

    removed unused script & fixed exit code 1

--- run_once_to_install_packages.sh
+++ run_once_to_install_packages.sh
@@ -4,26 +4,29 @@ cmd_exists() {
   command -v "$@" >/dev/null 2>&1
 }
 
-INSTALL_CMD="brew, pacman, dnf, apt"
+echo "attempting to install missing packages..."
+
+INSTALL="brew, pacman, dnf, apt"
 
 if cmd_exists brew; then
-  INSTALL_CMD="brew install"
+  INSTALL="brew install"
 elif cmd_exists pacman; then
-  INSTALL_CMD="sudo pacman -S"
+  INSTALL="sudo pacman -S"
 elif cmd_exists dnf; then
-  INSTALL_CMD="sudo dnf install"
+  INSTALL="sudo dnf install"
 elif cmd_exists apt; then
-  INSTALL_CMD="sudo apt install"
+  INSTALL="sudo apt install"
 else
-  echo "No package manager found, checked for ($INSTALL_CMD)" && exit
+  echo "No package manager found, checked for ($INSTALL)" && exit
 fi
 
 TO_INSTALL=( "git" "fd" "fzf" "lsd" "pnpm" "tmux" "zoxide" "bat" "micromamba" )
 
 # install neovim with nvim binary
-! cmd_exists nvim && eval "$INSTALL_CMD neovim"
+! cmd_exists nvim && eval "$INSTALL neovim"
 
 for PKG in "${TO_INSTALL[@]}"; do
-  ! cmd_exists "$PKG" && eval "$INSTALL_CMD $PKG"
+  ! cmd_exists "$PKG" && eval "$INSTALL $PKG"
 done
 
+exit 0
