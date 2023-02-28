35b776d870598703e7ba21ca803b3f84661a865c
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-02-28 10:11:29 +0100

    auto install for treesitter

--- dot_config/astronvim/lua/user/plugins/treesitter.lua
+++ dot_config/astronvim/lua/user/plugins/treesitter.lua
@@ -1,1 +1,21 @@
-return { "nvim-treesitter/nvim-treesitter", opts = { auto_install = true } }
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    auto_install = true,
+    ensure_installed = {
+      "rust",
+      "lua",
+      "python",
+      "javascript",
+      "typescript",
+      "markdown",
+      "markdown_inline",
+      "toml",
+      "json",
+      "yaml",
+      "html",
+      "help",
+      "gitcommit",
+    },
+  },
+}
