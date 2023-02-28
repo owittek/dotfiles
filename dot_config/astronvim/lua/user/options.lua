161dd400b0bfc8ee37de5ba067daea163d22c76f
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-02-28 10:09:22 +0100

    removed macos specific path & added spell checking

--- dot_config/astronvim/lua/user/options.lua
+++ dot_config/astronvim/lua/user/options.lua
@@ -1,12 +1,13 @@
 return {
   g = {
     loaded_perl_provider = 0,
-    python3_host_prog = "/opt/homebrew/bin/python3.10",
   },
   opt = {
     list = true,
     listchars = { tab = "│→", extends = "⟩", precedes = "⟨", trail = "·", nbsp = "␣" },
     wrap = true,
     linebreak = true,
+    spelllang = "en_us",
+    spellfile = vim.fn.expand "~/.config/astronvim/spell/en.utf-8.add",
   },
 }
