6413e1bf8e5552414b153f23028b9ebc39d87486
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-03-15 10:47:46 +0100

    feat(astronvim): added ts-rainbow2

--- dot_config/astronvim/lua/user/plugins/treesitter.lua
+++ dot_config/astronvim/lua/user/plugins/treesitter.lua
@@ -1,7 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = { "HiPhish/nvim-ts-rainbow2" },
   opts = {
-    auto_install = true,
+    auto_install = vim.fn.executable("tree-sitter") == 1,
+    indent = { enable = true, disable = { "python" } },
+    rainbow = { enable = true },
     ensure_installed = {
       "rust",
       "lua",
