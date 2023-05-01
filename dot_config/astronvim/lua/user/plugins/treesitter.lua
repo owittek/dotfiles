9fdcbdfab412b3686a57a938ba7661bfac25b92e
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-05-01 17:11:20 +0200

    chore(astronvim): remove not required configuration

--- dot_config/astronvim/lua/user/plugins/treesitter.lua
+++ dot_config/astronvim/lua/user/plugins/treesitter.lua
@@ -1,22 +1,23 @@
+local utils = require "astronvim.utils"
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = {
-    auto_install = vim.fn.executable "tree-sitter" == 1,
-    indent = { enable = true, disable = { "python" } },
-    ensure_installed = {
-      "rust",
+  opts = function(_, opts)
+    opts.auto_install = vim.fn.executable "tree-sitter" == 1
+
+    opts.indent = {
+      enable = true,
+      disable = { "python" },
+    }
+
+    if not opts.ensure_installed then opts.ensure_installed = {} end
+    utils.list_insert_unique(opts.ensure_installed, {
       "lua",
       "python",
-      "javascript",
-      "typescript",
       "markdown",
       "markdown_inline",
-      "toml",
-      "json",
       "yaml",
       "html",
-      "help",
       "gitcommit",
-    },
-  },
+    })
+  end,
 }
