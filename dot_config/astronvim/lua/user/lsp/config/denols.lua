2afcac1a482203b6536594915807863b2a99c0b8
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-05-01 17:09:30 +0200

    chore: remove not required files

--- dot_config/astronvim/lua/user/lsp/config/denols.lua
+++
@@ -1,4 +0,0 @@
-return function(opts)
-  opts.root_dir = require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")
-  return opts
-end
--- dot_config/astronvim/lua/user/lsp/setup_handlers.lua
+++
@@ -1,4 +0,0 @@
-return {
-  tsserver = function(_, opts) require("typescript").setup { server = opts } end,
-  denols = function(_, opts) require("deno-nvim").setup { server = opts } end,
-}
