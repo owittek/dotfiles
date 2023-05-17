9855ffdeab7f670aea8718bd1607a6500daac334
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-05-17 16:51:36 +0200

    feat: add wezterm.lua

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require "wezterm"
+return {
+  font = wezterm.font_with_fallback {
+    "JetBrainsMono Nerd Font",
+    "Symbols Nerd Font Mono",
+  },
+  font_size = 13,
+  color_scheme = "Catppuccin Mocha",
+  hide_tab_bar_if_only_one_tab = true,
+  window_close_confirmation = "NeverPrompt",
+  window_background_opacity = 0.9,
+  macos_window_background_blur = 5,
+  window_padding = {
+    left = "0cell",
+    right = "0cell",
+    top = "0.5cell",
+    bottom = "0cell",
+  },
+  window_decorations = "RESIZE",
+  audible_bell = "Disabled",
+}
