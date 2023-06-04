4aa1a9715596b0545b7a58f7bf441c742a2d4394
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-06-04 11:19:12 +0200

    feat(astronvim): add blackd-client to auto_install

--- dot_config/astronvim/lua/user/plugins/mason.lua
+++ dot_config/astronvim/lua/user/plugins/mason.lua
@@ -2,18 +2,12 @@ local utils = require "astronvim.utils"
 return {
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, {
-        "html",
-      })
-    end,
+    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "html" }) end,
   },
   {
     "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, {
-        "selene",
-      })
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "blackd-client" })
     end,
   },
 }
