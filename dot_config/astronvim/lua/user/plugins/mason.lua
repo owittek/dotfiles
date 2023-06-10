4184d41ad6e0752b5c216b9c510d0e54d9dd9f03
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-06-10 19:18:16 +0200

    feat(astronvim): use locally installed rust_analyzer instead of mason

--- dot_config/astronvim/lua/user/plugins/mason.lua
+++ dot_config/astronvim/lua/user/plugins/mason.lua
@@ -2,7 +2,16 @@ local utils = require "astronvim.utils"
 return {
   {
     "williamboman/mason-lspconfig.nvim",
-    opts = function(_, opts) opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "html" }) end,
+    opts = function(_, opts)
+      for index, lsp in ipairs(opts.ensure_installed) do
+        if lsp == "rust_analyzer" then
+          table.remove(opts.ensure_installed, index)
+          break
+        end
+      end
+
+      opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, { "html" })
+    end,
   },
   {
     "jay-babu/mason-null-ls.nvim",
