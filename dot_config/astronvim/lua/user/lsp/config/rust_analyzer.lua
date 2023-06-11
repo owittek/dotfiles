1861cd832cbf44d9a8e2aaf1ccc09f624585dbd8
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-06-11 16:42:48 +0200

    fix(astronvim): remove unused rust_analyzer settings

--- dot_config/astronvim/lua/user/lsp/config/rust_analyzer.lua
+++ dot_config/astronvim/lua/user/lsp/config/rust_analyzer.lua
@@ -5,12 +5,6 @@ return {
         command = "clippy",
         extraArgs = { "--no-deps" },
       },
-      procMacro = {
-        enable = true,
-      },
-      inlayHints = {
-        locationLinks = false,
-      },
     },
   },
 }
