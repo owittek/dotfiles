84fe9287bf794351ed987f902f2c5004da48ac63
Olivier Wittek <57495944+owittek@users.noreply.github.com>
2023-06-18 12:32:11 +0200

    refactor(astronvim): two locals into one

--- dot_config/astronvim/lua/user/highlights/init.lua
+++ dot_config/astronvim/lua/user/highlights/init.lua
@@ -1,6 +1,5 @@
 return function()
-  local get_hlgroup = require("astronvim.utils").get_hlgroup
-  local nontext = get_hlgroup "NonText"
+  local nontext = require("astronvim.utils").get_hlgroup "NonText"
   return {
     CursorLineFold = { link = "CursorLineNr" }, -- highlight fold indicator as well as line number
     GitSignsCurrentLineBlame = { fg = nontext.fg, italic = true }, -- italicize git blame virtual text
